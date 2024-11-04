10b70de6a637847c8ea2c6a0dfbb56192a5e6e95
David Garcia Maria <davidgama260402@gmail.com>
2024-11-04 23:17:04 +0100

    Commit inicial

---
+++ src/main/java/com/safa/enviovital/EnvioVitalApplication.java
@@ -0,0 +1,13 @@
+package com.safa.enviovital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioVitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvioVitalApplication.class, args);
+    }
+
+}
