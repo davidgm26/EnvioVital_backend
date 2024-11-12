7f76ca177037c006330cd6b5d92310df2ee9aa8a
Luis <lmarincarmona@safareyes.es>
2024-11-12 14:57:34 +0100

    arreglo de ProvinciaDTO

--- src/main/java/com/safa/enviovital/dto/ProvinciaDTO.java
+++ src/main/java/com/safa/enviovital/dto/ProvinciaDTO.java
@@ -6,11 +6,13 @@ import lombok.*;
 @Data
 @Builder
 public class ProvinciaDTO {
+    private Integer id;
     private String nombre;
 
 
     public static ProvinciaDTO createProvinciaDTOFromProvincia(Provincia provincia) {
         return ProvinciaDTO.builder()
+                .id(provincia.getId())
                 .nombre(provincia.getNombre())
                 .build();
     }
